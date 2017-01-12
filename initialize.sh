01a11dbbf9a04923684874eb866e1055dd23f9c8
Amir Hosseini <amirhs.hosseini@gmail.com>
2017-01-12 14:53:50 -0500

    added the initialization file for the disk image

---
+++ initialize.sh
@@ -0,0 +1,16 @@
+
+GROUP=node1-1.sb1.orbit-lab.org,node1-2.sb1.orbit-lab.org,node2-1.sb1.orbit-lab.org
+
+if [[ -n "$GROUP" ]]  
+then  
+  omf load -i geni-amirhsho-node-node1-1.sb1.orbit-lab.org-2017-01-12-14-46-26.ndz -t "$GROUP"
+else  
+  echo "Please set the GROUP variable"
+fi  
+
+if [[ -n "$GROUP" ]]  
+then  
+  omf tell -a on -t "$GROUP"
+else  
+  echo "Please set the GROUP variable"
+fi 

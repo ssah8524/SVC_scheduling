3a36eae0a644c704db01da5d10ad819dae5900f0
Amir Hosseini <amirhs.hosseini@gmail.com>
2017-01-16 16:41:59 -0500

    changed disk image

--- initialize.sh
+++ initialize.sh
@@ -3,7 +3,7 @@ GROUP=node1-1.sb1.orbit-lab.org,node1-2.sb1.orbit-lab.org,node2-1.sb1.orbit-lab.
 
 if [[ -n "$GROUP" ]]  
 then  
-  omf load -i geni-amirhsho-node-node1-1.sb1.orbit-lab.org-2017-01-12-14-46-26.ndz -t "$GROUP"
+  omf load -i geni-amirhsho-node-node1-5.sb4.orbit-lab.org-2017-01-16-16-38-38.ndz -t "$GROUP"
 else  
   echo "Please set the GROUP variable"
 fi  

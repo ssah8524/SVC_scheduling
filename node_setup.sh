0c318abf723ad4708b44bce28c71cb6bf1b3ec3d
Amir Hosseini <amirhs.hosseini@gmail.com>
2017-01-20 10:59:33 -0500

    node setup script added

---
+++ node_setup.sh
@@ -0,0 +1,11 @@
+
+cd SVC_scheduling
+git pull
+apt-get update
+apt-get -y install isc-dhcp-client
+modprobe ath5k
+#modprobe ath9k
+ifconfig wlan0 up
+wpa_passphrase node1-1 simpletest > wpa.conf
+wpa_supplicant -iwlan0 -cwpa.conf -B
+dhclient wlan0

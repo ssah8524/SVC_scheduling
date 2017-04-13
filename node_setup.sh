b744f1ccfab0c619f73a6ef343948c77a88b7e78
Amir Hosseini <amirhs.hosseini@gmail.com>
2017-04-13 14:23:25 -0400

    files modified

--- node_setup.sh
+++ node_setup.sh
@@ -3,8 +3,8 @@
 #git pull
 apt-get update
 apt-get -y install isc-dhcp-client
-#modprobe ath5k
-modprobe ath9k
+modprobe ath5k
+#modprobe ath9k
 ifconfig wlan0 up
 wpa_passphrase node1-1 demo_main > wpa.conf
 wpa_supplicant -iwlan0 -cwpa.conf -B

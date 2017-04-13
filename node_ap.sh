cbd5e320647465928247d598a6f9c6f11783207b
Amir Hosseini <amirhs.hosseini@gmail.com>
2017-04-13 15:34:18 -0400

    setup files altered

--- node_ap.sh
+++ node_ap.sh
@@ -1,17 +1,6 @@
-
-#cd SVC_scheduling
-#git pull
-apt-get update
-apt-get -y install isc-dhcp-client
-modprobe ath5k
-#modprobe ath9k
-ifconfig wlan0 up
-wpa_passphrase node1-1 demo_main > wpa.conf
-wpa_supplicant -iwlan0 -cwpa.conf -B
-dhclient wlan0
-
-
 ## For server side
+cd SVC_scheduling
+git pull
 git clone https://github.com/oblique/create_ap.git
 cd create_ap
 make install

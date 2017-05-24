59592451ad87a1d6a30cf670661c52190225413f
root <root@node1-1.outdoor.orbit-lab.org>
2017-05-24 15:01:43 -0400

    updated bash script

--- arp.sh
+++ arp.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-USERNO=$1
-IPPREFIX="192.168.0."
-b=1
-while [ $b -le $USERNO ];do
-    IFACE="$IPPREFIX$b"
-    for N in $(arp -n -i ap0 $IFACE | awk '{print $1","$3}' | grep -v "$IFACE" | tail -n +2);do
-        IP=$(echo $N | cut -f1 -d',')
-        MAC=$(echo $N | cut -f2 -d',')
-    done
-    let b=$(($b+1))
-done
 
+IFACE=ap0
+for N in $(arp -n -i $IFACE | awk '{print $1","$3}' | tail -n +2);do
+	IP=$(echo $N | cut -f1 -d',')
+        MAC=$(echo $N | cut -f2 -d',')
+	echo $MAC
+	sudo iw dev $IFACE station get $MAC
+done

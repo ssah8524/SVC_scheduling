4950155dd5827aac3c7f979bd4046e9918ea325a
Amir Hosseini <amirhs.hosseini@gmail.com>
2017-05-24 14:45:25 -0400

    bash script updated

--- arp.sh
+++ arp.sh
@@ -1,7 +1,13 @@
-
-
-IFACE=192.168.0.3
-for N in $(arp -n -i ap0 $IFACE | awk '{print $1","$3}' | grep -v "$IFACE" | tail -n +2);  do
-   IP=$(echo $N | cut -f1 -d',')
-   MAC=$(echo $N | cut -f2 -d',')
+#!/bin/bash
+USERNO=$1
+IPPREFIX="192.168.0."
+b=1
+while [ $b -le $USERNO ];do
+    IFACE="$IPPREFIX$b"
+    for N in $(arp -n -i ap0 $IFACE | awk '{print $1","$3}' | grep -v "$IFACE" | tail -n +2);do
+        IP=$(echo $N | cut -f1 -d',')
+        MAC=$(echo $N | cut -f2 -d',')
+    done
+    let b=$(($b+1))
 done
+

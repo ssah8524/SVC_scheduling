f8f47c67f83213791702196679063962b88b2741
Amir Hosseini <amirhs.hosseini@gmail.com>
2017-05-25 15:31:00 -0400

    unnecessary files deleted

--- arp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-IFACE=ap0
-for N in $(arp -n -i $IFACE | awk '{print $1","$3}' | tail -n +2);do
-	IP=$(echo $N | cut -f1 -d',')
-        MAC=$(echo $N | cut -f2 -d',')
-	echo $(sudo iw dev $IFACE station get $MAC | awk 'NR==9{print $2}')
-done
